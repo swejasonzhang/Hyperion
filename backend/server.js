593f5656640f5b797171d021cdd756fff267b83f
swejasonzhang <swejasonzhang@gmail.com>
2025-08-07 01:37:15 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("products", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(3000, () => {
+    connectDB();
+  console.log("Server is running on http://localhost:3000");
+});
