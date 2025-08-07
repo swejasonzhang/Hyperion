928fe516f4a96c0436bbb4f7f26b89bf8e95dc1a
swejasonzhang <swejasonzhang@gmail.com>
2025-08-07 09:00:00 -0400

    created product model and basic endpoint for products

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,20 @@ dotenv.config();
 
 const app = express();
 
-app.get("products", (req, res) => {});
+app.post("/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields.",
+    });
+  }
+});
 
 console.log(process.env.MONGO_URI);
 
 app.listen(3000, () => {
-    connectDB();
+  connectDB();
   console.log("Server is running on http://localhost:3000");
 });
