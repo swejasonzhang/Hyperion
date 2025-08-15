fbe8b00c3b69da6532132ca81838588b0fe2e8ac
swejasonzhang <swejasonzhang@gmail.com>
2025-08-14 21:14:31 -0400

    updating a product works as well and is updated in real time in atlas as well

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // allows us to acccpet JSON data in req.body
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.post("/api/products", async (req, res) => {
   const product = req.body;
@@ -30,6 +41,27 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product ID" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
 
@@ -37,6 +69,7 @@ app.delete("/api/products/:id", async (req, res) => {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
+    console.error("error in deleting product:", error.message);
     res.status(404).json({
       success: false,
       message: "Product not found",
