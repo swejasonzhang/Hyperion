ed20c3dfca50d86971222876d87d2c75aded7c76
swejasonzhang <swejasonzhang@gmail.com>
2025-08-14 20:47:59 -0400

    created products endpoint and used postman for verification

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.post("/products", async (req, res) => {
+app.use(express.json()); // allows us to acccpet JSON data in req.body
+
+app.post("/api/products", async (req, res) => {
   const product = req.body;
 
   if (!product.name || !product.price || !product.image) {
@@ -14,6 +17,16 @@ app.post("/products", async (req, res) => {
       success: false,
       message: "Please provide all fields.",
     });
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 });
 
