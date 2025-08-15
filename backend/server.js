996e4d2683fff4cde1272a2b9c3acbb0a8b963f8
swejasonzhang <swejasonzhang@gmail.com>
2025-08-14 21:00:02 -0400

    created deleting products by id then working on getting all products

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.post("/api/products", async (req, res) => {
     });
   }
 
-  const newProduct = new Product(product)
+  const newProduct = new Product(product);
 
   try {
     await newProduct.save();
@@ -27,6 +27,20 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.error("Error in Create product:", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
   }
 });
 
