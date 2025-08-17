cf4df2e30124e0e4da0a0d5a589ee959a15812ec
swejasonzhang <swejasonzhang@gmail.com>
2025-08-17 17:12:06 -0400

    working with react icons and chakra-ui on the frontend

--- frontend/src/components/ui/NavBar.jsx
+++ frontend/src/components/ui/NavBar.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { HiPlus } from "react-icons/hi";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+
+          <HStack spacing={2} alignItems={"center"}>
+            <Link to={"/create"}>
+              <Button>
+                <HiPlus size={20} />
+              </Button>
+            </Link>
+          </HStack>
+        </Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default NavBar;
