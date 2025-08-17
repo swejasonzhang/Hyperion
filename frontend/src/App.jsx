7963fe64f2d53942f7c45fe0a8f2fd7947b50f03
swejasonzhang <swejasonzhang@gmail.com>
2025-08-17 17:33:29 -0400

    working on create page for the foundatin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NavBar from "./components/ui/NavBar";
+import { useColorModeValue } from "./components/ui/color-mode";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/ui/NavBar.jsx
+++ frontend/src/components/ui/NavBar.jsx
@@ -1,8 +1,13 @@
 import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { HiPlus } from "react-icons/hi";
+import { useColorMode } from "./color-mode";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -16,22 +21,34 @@ const NavBar = () => {
       >
         <Text
           fontSize={{ base: "22", sm: "28" }}
-          fontWeight={"bold"}
-          textTransform={"uppercase"}
-          textAlign={"center"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
         >
-          <Link to={"/"}>Product Store 🛒</Link>
-
-          <HStack spacing={2} alignItems={"center"}>
-            <Link to={"/create"}>
-              <Button>
-                <HiPlus size={20} />
-              </Button>
-            </Link>
-          </HStack>
+          <Link
+            to="/"
+            style={{
+              background: "linear-gradient(to right, #06b6d4, #3b82f6)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+              display: "inline-block",
+            }}
+          >
+            Product Store 🛒
+          </Link>
         </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <HiPlus size={20} />
+            </Button>
+          </Link>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+          </Button>
+        </HStack>
       </Flex>
     </Container>
   );
